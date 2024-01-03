6eecb3b9844ed07781cd8c319622faeade217bf7
Adem <mr.admblck@gmail.com>
2024-01-03 14:11:33 +0300

    Değişiklikleri açıklayan bir mesaj

--- cepRaporu.js
+++ cepRaporu.js
@@ -1,12 +1,17 @@
 const { isUtf8 } = require('buffer');
 const express =require('express')
+const mongoose=require('mongoose')
+const cors=require('cors')
+
 const fs=require('fs')
 
 const app=express()
 
 const port=process.env.PORT || 3000
 
-app.use(express.json());
+app.use(cors())
+
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
